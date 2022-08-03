3e07d4d80f7a35ac8de497f1768b28104c5c3df2
VinogradovDanya <vinogradov.do@phystech.edu>
2022-08-03 10:32:45 +0300

    create testing

--- Charity/Charity/contracts/contracts/payment.sol
+++ Charity/Charity/contracts/contracts/payment.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity =0.8.15;
 
 contract Payments {
+    
 
     // @dev create a person: who want to donate in DAO(charity)
     struct Payment {
@@ -11,22 +12,26 @@ contract Payments {
         string message;
     }
 
-    //@dev create user's payment
+    // @dev create user's payment
     struct Balance {
         uint totalPayments;
         mapping(uint => Payment) payments;
     }
 
+    // @dev database users = charitiers
     mapping(address => Balance) public balances;
 
+    // @dev total value our treasury
     function currentBalance() public view returns(uint) {
         return address(this).balance;
     }
 
+    // @dev information about user's payment 
     function getPayment(address _addr, uint _index) public view returns(Payment memory) {
         return balances[_addr].payments[_index];
     }
 
+    // @dev pay in charity
     function pay(string memory message) public payable {
         uint paymentNum = balances[msg.sender].totalPayments;
         balances[msg.sender].totalPayments++;
